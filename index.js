8e015abcb81f3549093cbc4c750f12f28c82d60d
DKVI <92564346+DKVI@users.noreply.github.com>
2023-12-12 22:51:56 +0700

    commit

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ app.get("/image", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(".public/index.html");
+  const indexPath = path.join(__dirname, "public", "index.html");
+  fs.readFile(indexPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error reading index.html");
+    } else {
+      res.send(data);
+    }
+  });
 });
 app.listen(PORT, () => {
   console.log("Server is running in port " + PORT);
