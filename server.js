a553c84865c260dc3bc839b145f41d0ab4be29a8
DKVI <92564346+DKVI@users.noreply.github.com>
2023-12-12 22:11:49 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(express.static("public"));
+const PORT = 8080;
+app.get("/image", (req, res) => {
+  const fileName = req.query.img;
+  res.setHeader("Content-Type", "image/jpeg");
+  const image = fs.readFileSync(`./public/images/${fileName}`);
+  console.log(image);
+  res.send(image);
+});
+
+app.get("/", function (req, res) {
+  res.send("/index.html");
+});
+app.listen(PORT, () => {
+  console.log("Server is running in port " + PORT);
+});
